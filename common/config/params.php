854795939535f863c5e9410e06f6c14920cf2c59
Taras Shkodenko <taras.shkodenko@gmail.com>
2026-06-27 00:12:28 +0300

    Updated common config params.php

--- common/config/params.php
+++ common/config/params.php
@@ -1,12 +1,13 @@
 <?php
 
 return [
-    'adminEmail' => 'admin@example.com',
-    'supportEmail' => 'support@example.com',
-    'senderEmail' => 'noreply@example.com',
-    'senderName' => 'Example.com mailer',
+    'adminEmail' => 'taras.shkodenko@gmail.com',
+    'supportEmail' => 'taras.shkodenko@gmail.com',
+    'senderEmail' => 'noreply@go.shkodenko.com',
+    'senderName' => 'Go.shkodenko.com mailer',
     'user.passwordResetTokenExpire' => 3600,
     'user.passwordMinLength' => 8,
     'redirect.time' => 5,
     'app.urlPrefix' => 'https://go.shkodenko.com',
+    'app.name' => 'Redirector',
 ];
